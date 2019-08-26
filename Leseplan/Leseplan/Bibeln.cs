2ed5fc1970834c42f9569cc151013f1a81dec827
Jochen Kalmbach <jochen@kalmbachnet.de>
2019-08-26 22:55:17 +0200

    Order of bibles changed

--- Leseplan/Leseplan/Bibeln.cs
+++ Leseplan/Leseplan/Bibeln.cs
@@ -7,6 +7,12 @@ namespace Leseplan
     {
         public Bibeln()
         {
+            Translations.Add(new BibelCom { Short = "B-DELUT", Trans = "51", Text = "B: Luther 1912" });
+            Translations.Add(new BibelCom { Short = "B-HFA", Trans = "73", Text = "B: Hoffnung für alle" });
+            Translations.Add(new BibelCom { Short = "B-ELB", Trans = "57", Text = "B: Elberfelder 1905" });
+            Translations.Add(new BibelCom { Short = "B-SCH2000", Trans = "157", Text = "B: Schlachter 2000" });
+            Translations.Add(new BibelCom { Short = "B-NGU2011", Trans = "108", Text = "B: Neue Genfer Übersetzung" });
+
             Translations.Add(new BibelServer { Short = "LUT", Text = "BS: Lutherbibel 2017" });
             Translations.Add(new BibelServer { Short = "ELB", Text = "BS: Elberfelder Bibel" });
             Translations.Add(new BibelServer { Short = "HFA", Text = "BS: Hoffnung für Alle" });
@@ -16,12 +22,6 @@ namespace Leseplan
             Translations.Add(new BibelServer { Short = "NIV", Text = "BS: New International Version" });
             Translations.Add(new BibelServer { Short = "ESV", Text = "BS: English Standard Version" });
             Translations.Add(new BibelServer { Short = "ESV", Text = "BS: English Standard Version" });
-
-            Translations.Add(new BibelCom { Short = "B-DELUT", Trans = "51", Text = "B: Luther 1912" });
-            Translations.Add(new BibelCom { Short = "B-HFA", Trans = "73", Text = "B: Hoffnung für alle" });
-            Translations.Add(new BibelCom { Short = "B-ELB", Trans = "57", Text = "B: Elberfelder 1905" });
-            Translations.Add(new BibelCom { Short = "B-SCH2000", Trans = "157", Text = "B: Schlachter 2000" });
-            Translations.Add(new BibelCom { Short = "B-NGU2011", Trans = "108", Text = "B: Neue Genfer Übersetzung" });
         }
 
 
